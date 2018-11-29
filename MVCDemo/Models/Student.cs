783ca6ba2db1eced8f1fc125b8dac131982fb53c
Niteesh Yadav <niteesh.yadav@intsof.com>
2018-11-29 16:01:56 +0530

    CRUD functionality checkin.

--- MVCDemo/Models/Student.cs
+++ MVCDemo/Models/Student.cs
@@ -8,7 +8,7 @@
         public int ID { get; set; }
         public string LastName { get; set; }
         public string FirstMidName { get; set; }
-        public DateTime EnrollmentDate { get; set; }
+        public DateTime EnrollmentDate { get; set; }        
 
         public virtual ICollection<Enrollment> Enrollments { get; set; }
     }
